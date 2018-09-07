acf9925e5e0baa2df91872b6ce2c5ef783d18bab
Merline Belizaire <merline@noukod.com>
2018-09-07 17:07:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,146 +1,114 @@
-
-
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-# Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+class TicTacToe
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+  # Define your WIN_COMBINATIONS constant
+  WIN_COMBINATIONS = [
+    [0,1,2],  # Top row
+    [3,4,5],  # Middle row
+    [6,7,8],  # Bottom row
+    [2,4,6],  # Diagonal 1
+    [0,4,8],  # diagonal 2
+    [0,3,6],  # left
+    [1,4,7],  # Middle
+    [2,5,8]  # right
   ]
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-index =  user_input.to_i
-  index -= 1
-  return index
-end
-
-def move(board, index, current_player)
-  board[index] = current_player
-end
-
-def position_taken?(array, index)
-  if (array[index] == " " || array[index] == "" || array[index] == nil)
-    return false
-  else
-    return true
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-end
 
-def valid_move?(board, index)
-  if index.between?(0,8)
-    if position_taken?(board, index)
-      false
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def move(index, current_player = "X")
+    @board[index] = current_player
+  end
+
+  def position_taken?(location)
+    @board[location] != " " && @board[location] != ""
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
     else
-      true
-    end
-  else
-    false
-  end
-end
-
-def turn (board)
-  move_valid = false
-    until move_valid == true
-      puts "Please enter 1-9:"
-      index = input_to_index(gets.chomp)
-      move_valid = valid_move?(board,index)
-    end
-    move(board, index, current_player(board))
-    display_board(board)
-end
-
-def full?(board)
-	board.all?{|index| index == "X" || index == "O"}
-end
-
-def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  end
-    return false
-end
-
-def over?(board)
-  if won?(board) || full?(board) || draw?(board)
-    return true
-  else
-    return false
-  end
-end
-
-def winner (board)
-  index = []
-  index = won?(board)
-  if index == false
-    return nil
-  else
-    if board[index[0]] == "X"
-      return "X"
-    else
-      return "O"
+      turn
     end
   end
-end
 
-def turn_count board
-counter = 0
-board.each do |num|
-
-  if (num == "X" || num =="O")
-    counter = counter + 1
+  def turn_count
+    @board.count{|cell| cell == "X" || cell == "O"}
   end
-end
-return counter
-end
 
-def current_player(board)
-  turn_count(board)%2 == 0? "X" : "O"
-end
+  def current_player
+    turn_count.even? ? "X" : "O"
+  end
 
-def won?(board)
+  def won?
+     if @board.all? {|index| index == " "  }
+       false
+     else
+       WIN_COMBINATIONS.each do |win_combination|
 
-  WIN_COMBINATIONS.each do |win_combination|
-        win_index_1 = win_combination[0]
-        win_index_2 = win_combination[1]
-        win_index_3 = win_combination[2]
+         if win_combination.all? {|cell| @board[cell] == "X"} or win_combination.all? {|cell| @board[cell] == "O"}
+           return win_combination
+         end
 
-        position_1 = board[win_index_1] # load the value of the board at win_index_1
-        position_2 = board[win_index_2] # load the value of the board at win_index_2
-        position_3 = board[win_index_3] # load the value of the board at win_index_3
-
-        if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-           return win_combination # return the win_combination indexes that won.
         end
+        return false
+     end
   end
-  return false
-end
+  #full? retur true if board contains only X or O
+  def full?
+    @board.all? {|position| position == "X" or position == "O"}
+  end
 
+  def  draw?
+    if won? == false and full?
+      return true
+    else
+      return false
+    end
+  end
+  #over? return true if the game is draw or if is won
+  def over?
+     if draw? == true or won? != false
+       return true
+     end
+  end
 
-def play(board)
-until over?(board)
-  turn(board)
-end
-if winner(board) == "X"
-  puts "Congratulations X!"
-elsif winner(board) =="O"
-  puts "Congratulations O!"
-else
-  puts "Cat's Game!"
-end
+  def winner
+    if won? != false
+      @board[won?[0]]
+    end
+  end
+
+  #play
+  def play
+    #input = gets
+    until over? == true
+      turn
+    end
+
+    if winner == "X" or winner == "O"
+      puts "Congratulations #{winner}!"
+    else
+      puts "Cat's Game!"
+    end
+  end
+
 end
